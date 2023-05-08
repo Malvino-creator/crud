6c62805067712d317613afa1673bd0b856bb9d8f
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2023-05-09 00:39:11 +0300

    feat(Installed react bootstrap)

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button, Table} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Employees from './Employees';
+
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
